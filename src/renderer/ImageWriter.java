d02183e67e77d90ddb19848f0ee5ddb5d1859544
Lucas Benyounes <116970923+lucasbenyou@users.noreply.github.com>
2026-08-24 15:43:05 +0300

    Update ImageWriter.java
    Removed unnecessary author comment from ImageWriter class.

--- src/renderer/ImageWriter.java
+++ src/renderer/ImageWriter.java
@@ -14,8 +14,7 @@ import javax.imageio.*;
  * producing a non-optimized jpeg image from this matrix. The class although is
  * responsible of holding image related parameters of View Plane - pixel matrix
  * size and resolution
- * 
- * @author Dan
+ *
  */
 public class ImageWriter {
 	private int nX;
